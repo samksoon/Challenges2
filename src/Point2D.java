cfe8f01fedc06e50b6cfb75e44cf00a8b911e9a1
samksoon <samksoon2@gmail.com>
2015-03-30 18:54:34 -0400

    ysyghushgsh

---
+++ src/Point2D.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Point2D {
+	
+	private double x;
+	private double y;
+	
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	
+	public void setY(double y){
+		this.y = y;
+	}
+	public void setX(double x){
+		this.x = x;
+	}
+
+	public String toString() {
+		return x + ", " + y;
+	}
+}
---
+++ src/TestMain.java
@@ -0,0 +1,13 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		Point2D blah = new Point2D(1, 4);
+		System.out.println(blah);
+		blah.setX(15);
+		blah.setY(10);
+		System.out.println(blah);
+
+	}
+
+}
