b87a1b4c72142ec0e70ccb48400a997efef20f31
samksoon <samksoon2@gmail.com>
2015-04-02 16:47:48 -0400

    das
    das

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,9 +3,9 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		Point2D blah = new Point2D(1, 4);
-		Point2D other = new Point2D(4, 8);
+		Point2D other = new Point2D(1, 4);
 		Point2D gah = new Point2D(1, 4);
-		Point2D meep = new Point2D(3, 5);
+		Point2D meep = new Point2D(1, 5);
 		System.out.println(blah);
 		System.out.println(Geometry.distance(blah, other));
 		System.out.println(blah);
